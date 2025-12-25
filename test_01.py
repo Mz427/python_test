c73725adbdb435c826769cc38847e9e4907ccb36
mz_Ubuntu <zhuangzhimhao@gmail.com>
2025-12-25 19:59:03 +0800

    modified:   test_01.py

--- test_01.py
+++ test_01.py
@@ -2,22 +2,21 @@
 import os
 import re
 
-def rename_files(directory, str_search, str_old, str_new):
+def rename_files(directory, str_old, str_new):
     for filename_old in os.listdir(directory):
-        if re.search(rf'{str_search}', filename_old):
+        if re.search(rf'{str_old}', filename_old):
             filename_new = re.sub(rf'{str_old}', rf'{str_new}', filename_old)
             path_old = os.path.join(directory, filename_old)
             path_new = os.path.join(directory, filename_new)
             if not os.path.exists(path_new):
                 os.rename(path_old, path_new)
-                print(f"Renaming {filename_old} to {filename_new}")
+                print(f"Renaming '{filename_old}' to '{filename_new}'")
 
 # Example usage:
 # rename_files("/path/to/directory", "search", "old", "new")
 if __name__ == "__main__":
     dir_path = input("Enter the directory path: ")
-    str_search = input("Enter the search string: ")
     str_old = input("Enter the string to be replaced: ")
     str_new = input("Enter the new string: ")
-    rename_files(dir_path, str_search, str_old, str_new)
+    rename_files(dir_path, str_old, str_new)
     print("Files renamed successfully.")
