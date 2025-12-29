f5ea92fe83e02bc0e230ce7368cbc4d5d547d369
mz_Ubuntu <zhuangzhimhao@gmail.com>
2025-12-30 00:43:01 +0800

    modified:   test_00.py

--- test_00.py
+++ test_00.py
@@ -7,7 +7,7 @@ class TestHelloWorld(unittest.TestCase):
         self.l_example = [random.randint(1, 10) for _ in range(10)]
 
     def test_AlgorithmOfSorting(self):
-        self.assertEqual(algorithm_of_sorting.bubble_sort(self.l_example[:]), sorted(self.l_example))
+        self.assertEqual(sorted(self.l_example), algorithm_of_sorting.bubble_sort(self.l_example[:]))
 
 if __name__ == "__main__":
     unittest.main()
