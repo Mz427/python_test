b5db8407d032cc109336dc3aec5839bb8abd8079
mz_ubuntu <zhuangzhimhao@outlook.com>
2026-05-06 01:05:24 +0800

    modified:   .gitignore 	modified:   hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,13 @@
+import numpy
+
 class HelloWorld:
     def __init__(self, i):
         self.i = i
+
+    def say_hello_world(self):
+        print(self.i[::-1])
+        print(self.i)
+
+if __name__ == "__main__":
+    hello_world = HelloWorld([1, 2, 3, 4, 5])
+    hello_world.say_hello_world()
